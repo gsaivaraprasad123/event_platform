61c848bb5e17af1dc76d720e76fdc599af6fdd4b
Sai Vara Prasad Gandhe <gsaivaraprasad123@gmail.com>
2024-04-06 01:00:43 +0530

    Describe your changes here

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from 'next/cache'
 
-import { connectToDatabse } from '@/lib/database'
+import { connectToDatabase } from '@/lib/database'
 import User from '@/lib/database/models/user.model'
 import Order from '@/lib/database/models/order.model'
 import Event from '@/lib/database/models/event.model'
@@ -12,7 +12,7 @@ import { CreateUserParams, UpdateUserParams } from '@/types'
 
 export async function createUser(user: CreateUserParams) {
     try {
-        await connectToDatabse()
+        await connectToDatabase()
 
         const newUser = await User.create(user)
         return JSON.parse(JSON.stringify(newUser))
@@ -23,7 +23,7 @@ export async function createUser(user: CreateUserParams) {
 
 export async function getUserById(userId: string) {
     try {
-        await connectToDatabse()
+        await connectToDatabase()
 
         const user = await User.findById(userId)
 
@@ -36,7 +36,7 @@ export async function getUserById(userId: string) {
 
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
     try {
-        await connectToDatabse()
+        await connectToDatabase()
 
         const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true })
 
@@ -49,7 +49,7 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
 
 export async function deleteUser(clerkId: string) {
     try {
-        await connectToDatabse()
+        await connectToDatabase()
 
         // Find user to delete
         const userToDelete = await User.findOne({ clerkId })
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,16 +2,16 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || {conn:null ,promise:null}
+let cached = (global as any).mongoose || { conn: null, promise: null }
 
-export const connectToDatabse = async () => {
+export const connectToDatabase = async () => {
     if (cached.conn) return cached.conn
 
     if (!MONGODB_URI) throw new Error("MONGO_URI is missing");
 
     cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
         dbName: 'evently',
-        bufferCommands:false,
+        bufferCommands: false,
     })
 
     cached.conn = await cached.promise;
