401ecb8116a01b0b7575a6adc47149a173639ec5
Sai Vara Prasad Gandhe <gsaivaraprasad123@gmail.com>
2024-04-07 09:21:53 +0530

    mongo changes

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+type EventFormProps = {
+    userId: string
+    type:"Create" | "Update"
+}
+
+const EventForm = ({userId,type}:EventFormProps) => {
+  return (
+    <div>EventForm</div>
+  )
+}
+
+export default EventForm
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,20 +2,30 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn: null, promise: null }
+// let cached = (global as any).mongoose || { conn: null, promise: null }
+
+// export const connectToDatabase = async () => {
+//     if (cached.conn) return cached.conn
+
+//     if (!MONGODB_URI) throw new Error("MONGO_URI is missing");
+
+//     cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+//         dbName: 'evently',
+//         bufferCommands: false,
+//     })
+
+//     cached.conn = await cached.promise;
+
+//     return cached.conn;
+// }
 
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn
+    mongoose.set("strictQuery", true);
 
     if (!MONGODB_URI) throw new Error("MONGO_URI is missing");
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'evently',
-        bufferCommands: false,
-    })
 
-    cached.conn = await cached.promise;
-
-    return cached.conn;
+    mongoose.connect(MONGODB_URI)
+        .then(() => console.log("MongoDB connected"))
+        .catch((err) => console.log(err));
 }
-
